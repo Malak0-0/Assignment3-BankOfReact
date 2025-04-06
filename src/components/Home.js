c441ebd9d4af8707b9c3247711aefd62b8dcce79
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-04-06 17:02:18 -0400

    added navbar component and uodated home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 import './Home.css';
+import './Navbar.css';
 
 class Home extends Component {
   render() {
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'; 
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1 className="bank-name">Bank of React</h1>
+    <div className="nav-links">
+      <Link to="/">Home</Link>
+      <Link to="/userProfile">User Profile</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/credits">Credits</Link>
+      <Link to="/debits">Debits</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
