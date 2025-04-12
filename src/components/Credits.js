d1610ee29806868740578f8fc26d832af7368d72
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-04-12 13:28:30 -0400

    updated credit.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -4,9 +4,33 @@ src/components/Credits.js
 The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+//import AccountBalance from './AccountBalance';
+import './Credits.css';
+//import Navbar from './Navbar';
 
-const Credits = (props) => {
+
+
+
+const Credits = ({ credits, accountBalance, addCredit }) => {
+
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+
+
+  const handleFieldChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'description') setDescription(value);
+    else if (name === 'amount') setAmount(value);
+
+};
+
+
+
+
+
+
   return (
     <div>
       <h1>Credits</h1>
@@ -14,6 +38,7 @@ const Credits = (props) => {
       <Link to="/">Return to Home</Link>
     </div>
   );
-}
+};
+
 
 export default Credits;
